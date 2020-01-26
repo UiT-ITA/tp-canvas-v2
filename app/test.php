0a08e6bccf2fba17dacb1d2d7ee1079cc7fe679e
Håvard Pedersen <fuzzy76@fuzzy76.net>
2020-01-26 19:55:37 +0100

    Added test code for guzzle

--- app/test.php
+++ app/test.php
@@ -5,4 +5,16 @@
 
 require_once "global.php";
 
-phpinfo();
+use GuzzleHttp\Client;
+
+$client = new Client([
+    // Base URI is used with relative requests
+    'base_uri' => 'http://httpbin.org',
+    // You can set any number of default request options.
+    'timeout'  => 2.0,
+]);
+
+// Send a request to https://foo.com/root
+$response = $client->request('GET', '/root');
+
+var_dump($response);
