32e1e3df8cbd51d8916b35407e7bbaa2bc969bdd
Håvard Pedersen <fuzzy76@fuzzy76.net>
2020-01-28 21:14:59 +0100

    Wrong progress. Last was 185/627, this is 338/628.

--- app/tp-canvas-v2.php
+++ app/tp-canvas-v2.php
@@ -13,30 +13,104 @@ use PHPHtmlParser\Dom;
 
 $log->info("Starting run");
 
+$canvasHandlerStack = GuzzleHttp\HandlerStack::create();
+$canvasHandlerStack->push(GuzzleHttp\Middleware::retry(retryDecider(), retryDelay()));
 /** @todo pass on debug flag to the http client */
 $canvasclient = new GuzzleHttp\Client([
     'base_url' => "{$_SERVER['canvas_url']}api/v1/",
     'headers' => [
         'Authorization' => "Bearer {$_SERVER['canvas_key']}"
     ],
+    'handler' => $canvasHandlerStack,
     /** @todo fix exception support */
     'http_errors' => false // We are not exception compliant :-/
 ]);
 
+$tpHandlerStack = GuzzleHttp\HandlerStack::create();
+$tpHandlerStack->push(GuzzleHttp\Middleware::retry(retryDecider(), retryDelay()));
+/** @todo pass on debug flag to the http client */
+$tpclient = new GuzzleHttp\Client([
+    'base_url' => "{$_SERVER['tp_url']}ws/",
+    'headers' => [
+        'X-Gravitee-Api-Key' => $_SERVER['tp_key']
+    ],
+    'handler' => $tpHandlerStack,
+    /** @todo fix exception support */
+    'http_errors' => false // We are not exception compliant :-/
+]);
+
+function retryDecider()
+{
+    return function (
+        $retries,
+        Request $request,
+        Response $response = null,
+        RequestException $exception = null
+    ) {
+       // Limit the number of retries to 5
+        if ($retries >= 5) {
+            return false;
+        }
+
+        // Retry connection exceptions
+        if ($exception instanceof ConnectException) {
+            return true;
+        }
+
+        if ($response) {
+            // Retry on server errors
+            if ($response->getStatusCode() >= 500) {
+                return true;
+            }
+        }
+        return false;
+    };
+}
+function retryDelay()
+{
+    return function ($numberOfRetries) {
+        return 1000 * $numberOfRetries;
+    };
+}
+
 /**
  * Activerecord emulation wrapper class
  * @todo Replace with a proper wrapper
+ * @todo add properties from schema
  */
 class CanvasCourse
 {
     public function __construct()
     {
     }
+
+    public static function find_or_create(string $courseid)
+    {
+        /** @todo find or create new course */
+    }
+    public static function find(string $courseid) {
+        /** @todo find course by sis id */
+    }
+    public static function findBySisLike(string $like) {
+        /** @todo find courses where sis_course_id like '$like' */
+    }
+    public function delete()
+    {
+        /** @todo Delete event */
+    }
+    public function save()
+    {
+        /** @todo save event */
+    }
+    public function remove_all_canvas_events() {
+        /** @todo remove all canvas events for this course */
+    }
 }
 
 /**
  * Activerecord emulation wrapper class
  * @todo Replace with a proper wrapper
+ * @todo add properties from schema
  */
 class CanvasEvent
 {
@@ -44,8 +118,17 @@ class CanvasEvent
     {
     }
 
-    static public createNew(string $responsedata['id'], object $db_course) {
+    public static function createNew(string $canvasid, object $db_course)
+    {
         /** @todo Create new event record, add to chosen course */
+    }
+    public function delete()
+    {
+        /** @todo Delete event */
+    }
+    public function save()
+    {
+        /** @todo save event */
     }
 }
 
@@ -59,7 +142,8 @@ class CanvasEvent
  *
  * @return bool wether the events was "equal"
  */
-function tp_event_equals_canvas_event(array $tp_event, array $canvas_event, string $courseid) {
+function tp_event_equals_canvas_event(array $tp_event, array $canvas_event, string $courseid)
+{
     /** @todo There was thread seppuku flag detection code here. Needs to be redone if threading is implemented. */
 
     // If event is marked as deleted in canvas, pretend it's not there
@@ -82,7 +166,7 @@ function tp_event_equals_canvas_event(array $tp_event, array $canvas_event, stri
     // location
     $location = '';
     if (isset($tp_event['room']) && $tp_event['room']) {
-        $location = array_map(function($room) {
+        $location = array_map(function ($room) {
             return "{$room['buildingid']} {$room['roomid']}";
         }, $tp_event['room']);
         $location = implode(', ', $location);
@@ -160,8 +244,9 @@ function tp_event_equals_canvas_event(array $tp_event, array $canvas_event, stri
  *
  * @todo Ensure result is returned properly
  */
-function add_event_to_canvas(array $event, object $db_course, string $courseid, string $canvas_course_id) {
-    global $canvasclient;
+function add_event_to_canvas(array $event, object $db_course, string $courseid, string $canvas_course_id)
+{
+    global $log, $canvasclient;
 
     /** @todo There was thread seppuku flag detection code here. Needs to be redone if threading is implemented. */
 
@@ -169,7 +254,7 @@ function add_event_to_canvas(array $event, object $db_course, string $courseid, 
     $location = '';
     $map_url = '';
     if (isset($event['room']) && is_array($event['room'])) {
-        $location = array_map(function($room) {
+        $location = array_map(function ($room) {
             return "{$room['buildingid']} {$room['roomid']}";
         }, $event['room']);
         $location = implode(', ', $location);
@@ -201,7 +286,7 @@ function add_event_to_canvas(array $event, object $db_course, string $courseid, 
         }, $event['staffs']);
     }
     if (isset($event['xstaff-list']) && is_array($event['xstaff-list'])) {
-        $staff = array_merge($staff_arr, array_map(function ($staffp) { 
+        $staff = array_merge($staff_arr, array_map(function ($staffp) {
             if ($staffp['url'] != '') {
                 return "<a href='{$staffp['url']}'>{$staffp['name']} (ekstern)</a>";
             }
@@ -238,7 +323,7 @@ function add_event_to_canvas(array $event, object $db_course, string $courseid, 
     );
 
     // Send to Canvas
-    $response = $canvasclient->request('POST', 'calendar_events.json', [
+    $response = $canvasclient->post('calendar_events.json', [
         'json' => [
             'calendar_event' => [
                 'context_code' => "course_{$canvas_course_id}",
@@ -254,7 +339,225 @@ function add_event_to_canvas(array $event, object $db_course, string $courseid, 
     // Save to database if ok
     if ($response.getStatusCode() == 201) {
         $responsedata = json_decode($response.getBody(), true);
-        CanvasEvent::createNew($responsedata['id'], $db_course);    
+        CanvasEvent::createNew($responsedata['id'], $db_course);
+        $log->info("Event created in Canvas: {$title} - canvas id: {$responsedata['id']} - internal id: xxx");
     }
     /** @todo error handling and retries */
+}
+
+/**
+ * Delete single canvas event (db and canvas)
+ *
+ * @param object event database object of event
+ *
+ * @return void
+ * @todo implement error returns
+ */
+function delete_canvas_event(object $event)
+{
+    global $log, $canvasclient;
+    /** @todo There was thread seppuku flag detection code here. Needs to be redone if threading is implemented. */
+
+    $response = $canvasclient->delete("calendar_events/{$event->canvas_id}.json");
+    if ($response.getStatusCode() == 200) { // OK
+        event.delete();
+        $log->info("Event deleted in Canvas", ['event' => $event]);
+    } elseif ($response.getStatusCode() == 404) { // NOT FOUND
+        event.delete();
+        $log->warning("Event missing in Canvas", ['event'=>$event]);
+    } elseif ($response->getStatusCode() == 401) { // UNAUTHORIZED
+        // Is the event deleted in canvas?
+        $response = $canvasclient->get("calendar_events/{$event->canvas_id}.json");
+        $responsedata = json_decode($response.getBody(), true);
+        if ($responsedata['workflow_state'] == 'deleted') {
+            event.delete();
+            $log->warning("Event marked as deleted in Canvas", ['event'=>$event]);
+        } else {
+            $log->error("Unable to delete event in Canvas", ['event'=>$event]);
+        }
+    } else {
+        $log->error("Unable to delete event in Canvas", ['event'=>$event, 'error'=>$response->getStatusCode()]);
+    }
+}
+
+/**
+ * Delete all Canvas events for a course (db and canvas)
+ *
+ * @param object $course database course object
+ *
+ * @return void
+ * @todo implement error returns
+ */
+function delete_canvas_events(object $course)
+{
+    /** @todo this iterates over a magic property - needs implementation somehow */
+    foreach ($course->canvas_events as $event) {
+        delete_canvas_event($event);
+    }
+}
+
+/**
+ * Generic method to add a tp-timetable to one or more canvas courses
+ *
+ * @param array $courses canvas courselist from json
+ * @param array $tp_activities tp timetable from json
+ * @param string $courseid (e.g INF-1100)
+ *
+ * @return void
+ */
+function add_timetable_to_canvas(object $courses, object $tp_activities, string $courseid)
+{
+    if (!$tp_activities) {
+        return;
+    }
+
+    foreach ($courses as $course) {
+        // Find matching timetable
+        $actid = explode('_', $course['sis_course_id']);
+        $actid = end($actid);
+        $act_timetable = array_filter($tp_activities, function ($tp_act) use ($actid) {
+            return ($tp_act['actid'] == $actid);
+        });
+        add_timetable_to_one_canvas_course($course, $act_timetable, $courseid);
+    }
+}
+
+/**
+ * Add a tp timetable to one canvas course
+ *
+ * @param array $canvas_course from canvas json
+ * @param array $timetable from tp json
+ * @param string $courseid (e.g INF-1100)
+ *
+ * @return void
+ * @todo waaay too many error conditions to return
+ */
+function add_timetable_to_one_canvas_course(array $canvas_course, array $timetable, string $courseid)
+{
+    global $log, $canvasclient;
+
+    $db_course = CanvasCourse::find_or_create($canvas_course['id']);
+    $db_course->name = $canvas_course['name'];
+    $db_course->course_code = $canvas_course['course_code'];
+    $db_course->sis_course_id = $canvas_course['sis_course_id'];
+    $db_course.save();
+
+    // Empty tp-timetable
+    if (!$timetable) {
+        delete_canvas_events($db_course);
+        return;
+    }
+
+    // put tp-events in array (unnesting)
+    $tp_events = array();
+    foreach ($timetable as $t) {
+        foreach ($t['eventsequences'] as $eventsequence) {
+            foreach ($eventsequence['events'] as $event) {
+                $tp_events[] = $event;
+            }
+        }
+    }
+
+    // fetch canvas events found in db
+    /** @todo uses magic property - needs implementation */
+    foreach ($db_course->canvas_events as $canvas_event_db) {
+        /** @todo no error checking! */
+        $response = $canvasclient->get("calendar_events/{$canvas_event_db->canvas_id}.json");
+        $canvas_event_ws = json_decode($response.getBody(), true);
+        $found_matching_tp_event = false;
+
+        // Look for match between canvas and tp
+        foreach ($tp_events as $i => $tp_event) {
+            if (tp_event_equals_canvas_event($tp_event, $canvas_event_ws, $courseid)) {
+                // No need to update, remove tp_event from array of events
+                unset($tp_events[$i]);
+                $found_matching_tp_event = true;
+                $log->info(
+                    "Event match in TP and Canvas - no update needed",
+                    [
+                        'db_course' => $db_course,
+                        'canvas_event_db' => $canvas_event_db
+                    ]
+                );
+                break;
+            }
+        }
+
+        if (!$found_matching_tp_event) {
+            // Nothing matched in tp, this event has been deleted from tp
+            delete_canvas_event($canvas_event_db);
+        }
+    }
+
+    // Add remaining tp-events in canvas
+    foreach ($tp_events as $event) {
+        add_event_to_canvas($event, $db_course, $courseid, $canvas_course['id']);
+    }
+}
+
+/**
+ * Remove local courses that have been removed from Canvas
+ *
+ * @param array $canvas_courses
+ *
+ * @return void
+ */
+function remove_local_courses_missing_from_canvas(array $canvas_courses)
+{
+    foreach ($canvas_courses as $course_id) {
+        $local_course = CanvasCourse::find($course_id);
+        $local_course.remove_all_canvas_events();
+        $local_course.delete();
+    }
+}
+
+/**
+ * Check for structural changes in canvas courses.
+ * Only called explicitly from command line - called in cronjob
+ * 
+ * @param string $semester Semester string "YY[h|v]" e.g "18v"
+ */
+function check_canvas_structure_change($semester)
+{
+    global $log, $tpclient;
+
+    // Fetch all active courses from TP
+    $tp_courses = $tpclient->get("course", ['id' => $_SERVER['tp_institution'], 'sem' => $semester, 'times' => 1]);
+    if ($tp_courses.getStatusCode() != 200) {
+        $log->critical("Could not get course list from TP", array($semester));
+        return;
+    }
+    $tp_courses = json_decode($tp_courses.getBody(), true);
+
+    // For each course in tp...
+    foreach ($tp_courses['data'] as $tp_course) {
+        // Create Canvas SIS string
+        $sis_semester = make_sis_semester($semester, $tp_course['terminnr']);
+        // Fetch course candidates from Canvas
+        $canvas_courses = fetch_and_clean_canvas_courses($tp_course['id'], $semester, $tp_course['terminnr'], false);
+        $canvas_courses_ids = array_column($canvas_courses, 'sis_course_id');
+
+        // ? Seems to collect sis_course_id for all courses that we have touched that matches
+        /** @todo verify this like syntax */
+        $local_courses = CanvasCourse::findBySisLike("%{$tp_course['id']}\\_%\\_{$sis_semester}%");
+        $local_courses = array_column($local_courses, 'sis_course_id');
+
+        // Gather id's that only exist in one of the arrays?
+        $local_diff = array_diff($local_courses, $canvas_courses_ids);
+        $canvas_diff = array_diff($canvas_courses_ids, $local_courses);
+        $diff = array_merge($local_diff, $canvas_diff);
+
+        if ($local_diff) {
+            // Local courses that does not exist in canvas any more
+            remove_local_courses_missing_from_canvas($local_diff);
+            $log->warning('Local course removed from canvas', ['tp_course' => $tp_course, 'semester' => $semester]);
+            /** @todo there used to be a sentry event here as well? */
+        }
+
+        if ($diff) {
+            // Courses in canvas that we have no trace of locally
+            /** @todo should this really have been $canvas_diff ? */
+            $log->warning('Course changed in canvas and need to update', ['tp_course' => $tp_course, 'semester' => $semester]);
+        }
+    }
 }
